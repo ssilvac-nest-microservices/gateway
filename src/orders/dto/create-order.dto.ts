7857fb601b6c31640296c7390ce02cbcccbbae41
Sebastián Silva <sebasilvac88@gmail.com>
2024-04-07 21:54:45 -0400

    add order details

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,28 +1,11 @@
-import {
-  IsBoolean,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-} from 'class-validator';
-import { OrderStatus, OrderStatusList } from '../enum/orderStatus.enum';
+import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { OrderItemDto } from '.';
 
 export class CreateOrderDto {
-  @IsNumber()
-  @IsPositive()
-  totalAmount: number;
-
-  @IsNumber()
-  @IsPositive()
-  totalItems: number;
-
-  @IsEnum(OrderStatus, {
-    message: `Invalid status. Must be one of the following values: ${OrderStatusList}`,
-  })
-  @IsOptional()
-  status: OrderStatus = OrderStatus.PENDING;
-
-  @IsBoolean()
-  @IsOptional()
-  paid: boolean = false;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
 }
--- src/orders/dto/index.ts
+++ src/orders/dto/index.ts
@@ -1,3 +1,4 @@
 export * from './create-order.dto';
+export * from './order-item.dto';
 export * from './pagination-order.dto';
 export * from './status.dto';
